b2c7cc0265046771b521f211cef6076e8469cb6e
Raphael <raphael.gisler@agilita.ch>
2026-07-28 06:38:05 +0200

    Add workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,20 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            System.getenv("KEYSTORE_FILE")?.let {
+                storeFile = file(it)
+                storePassword = System.getenv("KEYSTORE_PASSWORD")
+                keyAlias = System.getenv("KEY_ALIAS")
+                keyPassword = System.getenv("KEY_PASSWORD")
+            }
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
 
