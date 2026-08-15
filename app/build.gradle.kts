8d5d27ca351464da66576173cae3f21194b1595f
kenji <raphael-gi@users.noreply.github.com>
2026-08-15 13:51:17 +0200

    Bump Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.kenji.food.tracker"
         minSdk = 33
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
